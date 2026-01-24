2a8db160bcac7510196d6a1a7c26a91fd3bd01dd
Harfragot <ilorcag@uft.edu>
2026-01-24 00:56:46 -0300

    Slash and slash sound

--- PEI II Proyect/Assets/Script/Player/AttackController.cs
+++ PEI II Proyect/Assets/Script/Player/AttackController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,6 +9,7 @@ public class AttackController : MonoBehaviour
     private bool inputAttack = false;
 
     private PlayerController playerController;
+    private AudioSource audioSource;
 
     private int count = 0;
 
@@ -17,7 +19,15 @@ public class AttackController : MonoBehaviour
     [SerializeField] private float radiusAttack = 3f;
 
     [Header("Referencias")]
+
+    [Header("Audio")]
+    [SerializeField] private AudioClip slashAudio;
+
     [SerializeField] private ParticleSystem slashVFX;
+    [SerializeField] private ParticleSystem slashVFX2;
+    [SerializeField] private ParticleSystem slashVFX3;
+
+    [SerializeField] private ParticleSystem sparkVFX;
 
     public enum Attack { A = 0, B = 1 , C = 2}
     public Attack actualAttack = Attack.A;
@@ -27,6 +37,7 @@ public class AttackController : MonoBehaviour
     {
         playerController = GetComponent<PlayerController>();
         SwordPrefab.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
     }
     private void Update()
     {
@@ -36,14 +47,17 @@ public class AttackController : MonoBehaviour
         }
         if(!isAttacking && inputAttack && actualAttack == Attack.A && playerController.playerStats.haveSword)
         {
+            audioSource.PlayOneShot(slashAudio);
             StartCoroutine(AttackAPerformance());
         }
         if(!isAttacking && inputAttack && actualAttack == Attack.B && playerController.playerStats.haveSword)
         {
+            audioSource.PlayOneShot(slashAudio);
             StartCoroutine(AttackBPerformance());
         }
         if(!isAttacking && inputAttack && actualAttack == Attack.C && playerController.playerStats.haveSword)
         {
+            audioSource.PlayOneShot(slashAudio);
             StartCoroutine(AttackCPerformance());
         }
         if(count >= 4)
@@ -79,20 +93,24 @@ public class AttackController : MonoBehaviour
                     BasicKnight enemy = hits[i].gameObject.GetComponent<BasicKnight>();
                     enemy.TakeDamage(this.damage);
                     StartCoroutine(enemy.TakeKnockBack());
+                    sparkVFX.Play();
                     break;
                 case "SlugBomb":
                     SlugBomb slugEnemy = hits[i].gameObject.GetComponent<SlugBomb>();
                     slugEnemy.TakeDamage(this.damage);
                     StartCoroutine(slugEnemy.TakeKnockBack());
+                    sparkVFX.Play();
                     break;
                 case "Grass":
                     GrassController grass = hits[i].gameObject.GetComponent<GrassController>();
                     GameplayManager.instance.DropItems(grass.transform, grass.maxSepiasAmount);
                     Destroy(grass.gameObject);
+                    sparkVFX.Play();
                     break;
                 case "OctoRock":
                     OctoRock octoRock = hits[i].gameObject.GetComponent<OctoRock>();
                     octoRock.TakeDamage(this.damage);
+                    sparkVFX.Play();
                     break;
             }
         }
@@ -104,7 +122,7 @@ public class AttackController : MonoBehaviour
     private IEnumerator AttackBPerformance()
     {
         isAttacking = true;
-        slashVFX.Play();
+        slashVFX2.Play();
 
 
         Collider[] hits = Physics.OverlapSphere(transform.position, radiusAttack);
@@ -117,20 +135,24 @@ public class AttackController : MonoBehaviour
                     BasicKnight enemy = hits[i].gameObject.GetComponent<BasicKnight>();
                     enemy.TakeDamage(this.damage);
                     StartCoroutine(enemy.TakeKnockBack());
+                    sparkVFX.Play();
                     break;
                 case "SlugBomb":
                     SlugBomb slugEnemy = hits[i].gameObject.GetComponent<SlugBomb>();
                     slugEnemy.TakeDamage(this.damage);
                     StartCoroutine(slugEnemy.TakeKnockBack());
+                    sparkVFX.Play();
                     break;
                 case "Grass":
                     GrassController grass = hits[i].gameObject.GetComponent<GrassController>();
                     GameplayManager.instance.DropItems(grass.transform, grass.maxSepiasAmount);
                     Destroy(grass.gameObject);
+                    sparkVFX.Play();
                     break;
                 case "OctoRock":
                     OctoRock octoRock = hits[i].gameObject.GetComponent<OctoRock>();
                     octoRock.TakeDamage(this.damage);
+                    sparkVFX.Play();
                     break;
             }
         }
@@ -143,7 +165,7 @@ public class AttackController : MonoBehaviour
     private IEnumerator AttackCPerformance()
     {
         isAttacking = true;
-        slashVFX.Play();
+        slashVFX3.Play();
 
         Collider[] hits = Physics.OverlapSphere(transform.position, radiusAttack);
 
@@ -155,20 +177,24 @@ public class AttackController : MonoBehaviour
                     BasicKnight enemy = hits[i].gameObject.GetComponent<BasicKnight>();
                     enemy.TakeDamage(this.damage);
                     StartCoroutine(enemy.TakeKnockBack());
+                    sparkVFX.Play();
                     break;
                 case "SlugBomb":
                     SlugBomb slugEnemy = hits[i].gameObject.GetComponent<SlugBomb>();
                     slugEnemy.TakeDamage(this.damage);
                     StartCoroutine(slugEnemy.TakeKnockBack());
+                    sparkVFX.Play();
                     break;
                 case "Grass":
                     GrassController grass = hits[i].gameObject.GetComponent<GrassController>();
                     GameplayManager.instance.DropItems(grass.transform, grass.maxSepiasAmount);
                     Destroy(grass.gameObject);
+                    sparkVFX.Play();
                     break;
                 case "OctoRock":
                     OctoRock octoRock = hits[i].gameObject.GetComponent<OctoRock>();
                     octoRock.TakeDamage(this.damage);
+                    sparkVFX.Play();
                     break;
             }
         }
